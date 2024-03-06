773b5d20f0c1eafd3341947e346f02e0b79ab981
gaesyeah <gabrielgoesrios@gmail.com>
2024-03-06 16:24:28 -0300

    chore: apply reset and global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import ResetStyle from "./styles/ResetStyle.ts";
+import GlobalStyle from "./styles/GlobalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
+
     <App />
   </React.StrictMode>
 );
